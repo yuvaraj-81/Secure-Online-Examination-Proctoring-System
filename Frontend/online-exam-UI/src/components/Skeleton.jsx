9072bc7a92a7ae1e586d813256710bfd2f75b7f5
Yuvaraj S <yuvarajsubramani8105@gmail.com>
2026-01-08 11:59:08 +0530

    Update in UI

--- Frontend/online-exam-UI/src/components/Skeleton.jsx
+++ Frontend/online-exam-UI/src/components/Skeleton.jsx
@@ -1,7 +1,22 @@
 import "./Skeleton.css";
 
-const Skeleton = ({ height = 20 }) => {
-  return <div className="skeleton" style={{ height }} />;
+const Skeleton = ({
+  height = 20,
+  width = "100%",
+  radius = 8,
+  style = {},
+}) => {
+  return (
+    <div
+      className="skeleton"
+      style={{
+        height,
+        width,
+        borderRadius: radius,
+        ...style,
+      }}
+    />
+  );
 };
 
 export default Skeleton;
